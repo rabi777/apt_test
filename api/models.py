d0e658704726527875e0259a588d5cb9f9b5d34f
rabi777 <rabi375306@gmail.com>
2019-03-30 15:54:03 +0600

    count request

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Counter(models.Model):
+    data_request = models.IntegerField(default=1)
+
+    def __int__(self):
+        return int(self.data_request)
--- api/views.py
+++ api/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.views.generic.base import View
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from api.models import Counter
+
 
 class DataListApiView(APIView):
     def get(self, request):
-        data = ['a', 'b', 'c', 'd', 'e', 'f']
+        ar = ['a', 'b', 'c', 'd', 'e', 'f']
+        try:
+            get_count = Counter.objects.get(id=1)
+            count = get_count.data_request
+            get_count.data_request = count + 1
+            get_count.save()
+            obj = Counter.objects.get(id=1).data_request
+            print(obj)
+        except:
+            Counter.objects.create()
+            obj = 1
+
+        data = {
+            'data': ar,
+            'counter': obj
+        }
         return Response(data)
 
 
